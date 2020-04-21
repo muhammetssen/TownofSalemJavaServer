9e49c0ed2f7217818b973aa8c44252430bfc7c13
muhammetssen <enmuhammet23@gmail.com>
2020-04-21 16:56:37 +0300

    User class has its own file now

---
+++ User.java
@@ -0,0 +1,19 @@
+import java.io.FileNotFoundException;
+
+public class User {
+    public UserThread myThread;
+    public String userName;
+    public User(UserThread myThread){
+        this.myThread = myThread;
+        Server.userThreads.add(myThread);
+        Server.threadDictionary.put(myThread, this);
+    }
+
+    public void removeUser() throws FileNotFoundException {
+        System.out.print(this.myThread.getUserName()+ " is removing... ");
+        Server.threadDictionary.remove(this.myThread);
+        Server.userThreads.remove(this.myThread);
+        System.out.println("\t done");
+        Server.logger(this.myThread.socket.getInetAddress() +" username: "+this.myThread.getUserName()  + " disconnected");
+    }
+}
