8606f44a46497a53cb20e3bed31338fbf3687610
Muhammet Şen <enmuhammet23@gmail.com>
2020-04-22 19:03:00 +0300

    When host leaves, another player will be chosed.

--- Game.java
+++ Game.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.net.SocketException;
 import java.util.*;
 
 public class Game {
@@ -10,25 +11,57 @@ public class Game {
     public Game(User gameHost, Server server) throws NumberFormatException, IOException { // User gameHost
         ArrayList<String> types = new ArrayList<String>(); //["Villager", "Villager","Villager","Vampire","Vampire"];
         this.server = server;
+        gameHost.myThread.sendMessage("You are the Host");
+        String decidedTypes = "There will be ";
         for (String type : playerTypes) {
             //HOSTA TEK TEK KACAR TANE OLACAGINI SOR INPUT AL
-            gameHost.myThread.server.broadcast("How many " + type );
-            System.out.println("emre1");
-            int count = Integer.parseInt(gameHost.myThread.reader.readLine());
-            this.server.broadcast(count+"");
-            System.out.println("emre2");
+            gameHost.myThread.sendMessage("How many " + type+"s do you want?" );
+            boolean isValid = false;
+            int count= 0;
+            while(!isValid){
+                
+                try {
+                    String input  ;
+                    if((input = gameHost.myThread.readFromUser()) == null){
+                        this.server.broadcast("HOST HAS LEFT THE GAME!!");
+                        gameHost.removeUser();
+                        if(Server.userThreads.size() != 0){
+                            gameHost = Server.threadDictionary.keys().nextElement().user;
+                            gameHost.myThread.sendMessage("You are the New Host");
+                            gameHost.myThread.sendMessage("How many " + type+"s do you want?" );
+                            continue;
+                        }
+                        else{
+                            System.out.println("NO ONE LEFT. CLOSING THE GAME!");
+                            System.exit(0);
+                            break;
+                        }
+                    }
+                        
+                    count = Integer.parseInt(input);
+                    isValid = true;
+                    gameHost.myThread.sendMessage("Okay, "+count + " is valid.");
+
+                    
+                } catch (Exception e) {
+                    gameHost.myThread.sendMessage("Please enter an Integer");
+                }
+            }
+
+           
+            decidedTypes += count + " " + type+"s, " ;
             for (int i = 0; i < count; i++) 
                 types.add(type);
         }
-        System.out.println(types);
-        this.taker(types.toString()); //TODO CIRKIN YAZDIRIYOR
-
+        //System.out.println(types);
+        //this.taker(types.toString()); //TODO CIRKIN YAZDIRIYOR
+        this.informer(decidedTypes.substring(0, decidedTypes.length()-2) + ".");
 
         
     } 
-    public  void taker(String a){
+    public  void informer(String a){
         System.out.println("Game is saying " +a);
-        this.server.broadcast("Server is sending " + a);
+        this.server.broadcast("SERVER> " + a);
     }
     public void analyze(String komut){
         System.out.println("kadir is yapacak");
