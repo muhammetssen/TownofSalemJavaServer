6b8102c8f6961f7bbcdd3fb97251873ece2dc4c6
Muhammet Şen <enmuhammet23@gmail.com>
2020-04-26 00:10:37 +0300

    Do not accept new connections when the game starts.

--- Main/Server.java
+++ Main/Server.java
@@ -10,12 +10,19 @@ import User.*;
 public class Server {
     private int port;
     public Game game;
+    public ServerSocket serverSocket;
 
     public static ArrayList<UserThread> userThreads = new ArrayList<UserThread>();
     public static Hashtable<UserThread, User> threadDictionary = new Hashtable<UserThread, User>();    
     public static ArrayList<String> userNames = new ArrayList<String>(); 
+    public static Hashtable<String, User> userNameDictionary = new Hashtable<String, User>();    
+    
+    public boolean isEveryoneReady = false;
     public Server(int port){
         this.port = port;
+
+
+    
     }
 
     
@@ -29,6 +36,7 @@ public static void main(String[] args) {
     Server server = new Server(Integer.parseInt(args[0]));
     //If possible, create a ServerSocket object with given port
     try(ServerSocket serverSocket = new ServerSocket(server.port)){
+        server.serverSocket = serverSocket;
         System.out.println("Server is starting on port " + server.port);
         Socket hostSocket = serverSocket.accept();
         UserThread hostThread = new UserThread(hostSocket,server);
@@ -36,14 +44,20 @@ public static void main(String[] args) {
         Host hostUser = new Host(hostThread);
         hostThread.user = hostUser;
             
-        while(true){
-            Socket incomingSocket = serverSocket.accept(); //ClientSocket           
-            UserThread newUserThread = new UserThread(incomingSocket, server);
-            newUserThread.start();
-            User newUser = new User(newUserThread);
-            newUserThread.user = newUser;
-
+        while(!server.isEveryoneReady){
+            try {
+                Socket incomingSocket = serverSocket.accept(); //ClientSocket           
+                UserThread newUserThread = new UserThread(incomingSocket, server);
+                newUserThread.start();
+                User newUser = new User(newUserThread);
+                newUserThread.user = newUser;
+            } catch (SocketException e) {
+                System.out.println("Socket is closed");
+            }
         }
+        System.out.println("Everyone is ready!!");
+        server.broadcast("Everyone is ready! The game is starting");
+
     }
     catch(IOException ex){
         System.out.println("Server Error" + ex);
