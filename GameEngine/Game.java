9002422b8fbb7741f31b689daab8b356ff638df5
Muhammet Şen <enmuhammet23@gmail.com>
2020-04-26 00:09:37 +0300

    !ready and !pm commands

--- GameEngine/Game.java
+++ GameEngine/Game.java
@@ -1,5 +1,6 @@
 package GameEngine;
 import java.io.IOException;
+import java.lang.reflect.Type;
 import java.util.*;
 import User.*;
 import Main.*;
@@ -8,9 +9,9 @@ public class Game {
     public ArrayList<User> users = new ArrayList<User>();
     public User gameHost;
     public ArrayList<String> playerTypes = new ArrayList<String>(Arrays.asList("Villager", "Vampire"));
+    public ArrayList<String> types = new ArrayList<String>(); //["Villager", "Villager","Villager","Vampire","Vampire"];
     public Server server;
     public Game(User gameHost, Server server) throws NumberFormatException, IOException { // User gameHost
-        ArrayList<String> types = new ArrayList<String>(); //["Villager", "Villager","Villager","Vampire","Vampire"];
         this.server = server;
         gameHost.myThread.sendMessage("You are the Host");
         String decidedTypes = "There will be ";
@@ -20,18 +21,16 @@ public class Game {
             boolean isValid = false;
             int count= 0;
             while(!isValid){
-                
                 try {
                     String input  ;
                     if((input = gameHost.myThread.readFromUser()) == ""){
                         this.server.broadcast("HOST HAS LEFT THE GAME!!");
                         System.exit(0);
                         /**TODO A random player will be chosen as host, look after hav'ng a host GUI */
-                    }
-                        
+                    }   
                     count = Integer.parseInt(input);
                     isValid = true;
-                    gameHost.myThread.sendMessage("Okay, "+count + " is valid.");
+                    gameHost.myThread.sendMessage("Okay, "+count+" " + type+"(s).");
 
                     
                 } catch (Exception e) {
@@ -44,18 +43,45 @@ public class Game {
             for (int i = 0; i < count; i++) 
                 types.add(type);
         }
-        //System.out.println(types);
-        //this.taker(types.toString()); //TODO CIRKIN YAZDIRIYOR
-        this.informer(decidedTypes.substring(0, decidedTypes.length()-2) + ".");
-
-        
+         //TODO CIRKIN YAZDIRIYOR
+         this.server.broadcast("SERVER> " + decidedTypes.substring(0, decidedTypes.length()-2) + ".");  
     } 
-    public  void informer(String a){
-        System.out.println("Game is saying " +a);
-        this.server.broadcast("SERVER> " + a);
-    }
-    public void analyze(String komut){
-        System.out.println("kadir is yapacak");
+    public void analyze(User sender,String command) throws IOException {
+        if(command.length() <=1) return;
+        
+        switch (command.substring(1).toLowerCase().split(" ")[0]) {
+            case "ready":
+                System.out.println(sender.userName +" is ready");
+                sender.isReady = true;
+                boolean isGameReady = Server.threadDictionary.size() ==  this.types.size();
+                int readyCount = 0;
+                for (User user : Server.threadDictionary.values()){
+                    isGameReady = isGameReady && user.isReady;
+                    if(user.isReady) readyCount++;
+                }
+                sender.myThread.sendMessage("You are marked as Ready. Waiting for others.\t" + readyCount + "/" + Server.userThreads.size());
+                if(readyCount > types.size()) server.broadcast("More players than host configured." + (readyCount-types.size()) +" player(s) must leave."  );
+                if(isGameReady){
+                    this.server.isEveryoneReady = true;
+                    this.server.serverSocket.close();
+                }
+                break;
+            case "pm":
+                Scanner reader = new Scanner(command);
+                reader.next(); // skipping pm
+                if(!reader.hasNext()) return;
+                String destUser = UserThread.capitalize(reader.next());
+                System.out.println(destUser);
+                if(!reader.hasNext()) return;
+                String message = "PM "+ sender.userName +"> "+  reader.nextLine(); 
+                System.out.println(message);
+                if(!Server.userNameDictionary.keySet().contains(destUser)) return;
+                Server.userNameDictionary.get(destUser).myThread.sendMessage(message);
+                break; 
+            default:
+                break;
+        }
+        //System.out.println("kadir is yapacak");
     }
   
     }
